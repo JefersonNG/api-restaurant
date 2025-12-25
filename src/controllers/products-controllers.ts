3ce80944d50545f3ac893827f54d81d0048b5d85
jeferson nascimento <jeffersom732@gmail.com>
2025-12-25 18:12:03 -0400

    config app error, zod, knex

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -1,10 +1,25 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "@/utils/AppError";
+import { z } from "zod";
 
 export class ProductController {
   async index(Request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "ola" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(4),
+        price: z.number().gt(0),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      return response.json({ name, price });
     } catch (error) {
       next(error);
     }
